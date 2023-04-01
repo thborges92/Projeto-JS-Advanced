dafa8fe3a7a84d734fd7facafe0556887354bc16
Thiago Borges <thborges92@hotmail.com>
2023-04-01 12:25:36 -0300

    atualizando

--- index.js
+++ index.js
@@ -7,22 +7,11 @@ const routes = require('./ROUTES/routes')
 const dotenv = require('dotenv');
 const port = 3000;
 
-dotenv.config(); //identifica que o arquivo .env
+dotenv.config();
 connectToDatabase();
 
 app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public"))); //achar a pasta public
-app.use(express.urlencoded({ extend: true })); //ler os aquivos dentro da pasta public
+app.use(express.static(path.join(__dirname, "public"))); 
+app.use(express.urlencoded({ extend: true })); 
 app.use(routes);
-
-//rota de teste de requicisação get
-// app.get('/', function (req, res) {
-//     res.send('Olá Mundo!!');
-// });
-
-//rota para renderizar a página principal do site(aplicação) - index.ejs
-// app.get('/', function (req, res) { http://localhost:3000/home
-//     res.render('index');
-// })
-
 app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}/`));
